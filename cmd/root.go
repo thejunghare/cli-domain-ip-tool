fb7274526902cfb8fd17f6861264b428c4e59575
prasad-nimap <prasadjunghare@nimapinfotech.com>
2023-07-06 12:34:46 +0530

    updated short for root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -4,5 +4,5 @@ import "github.com/spf13/cobra"
 
 var RootCmd = &cobra.Command{
 	Use:   "domain-ip-tool",
-	Short: "A CLI tool to find IP addresses from domains",
+	Short: "A CLI tool is used to find IP addresses from domains.",
 }
